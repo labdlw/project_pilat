84682fbe0fc1efc13453de4c0f60e20ff93968cd
labdlw <160532925+labdlw@users.noreply.github.com>
2025-05-02 16:17:30 +0200

    Update 02_stacks_denovo_pipeline.sh

--- wet-meadow-connectivity/scripts/radseq_pipeline/02_stacks_denovo_pipeline.sh
+++ wet-meadow-connectivity/scripts/radseq_pipeline/02_stacks_denovo_pipeline.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -l
+#SBATCH --nodes=1 
+#SBATCH --time=150:00:00   
+#SBATCH --partition=smp-rh7
+#SBATCH --cpus-per-task=8 
+#SBATCH --mem=46gb
+
+
+module load stacks/2.65 
+
+denovo_map.pl --samples ../sh_samples/ --popmap ../popmap_sh.txt --out-path ../denovom3_M1_n1/ --paired -m 3 -M 1 -n 1 -T 16 --catalog-popmap ./catalog_sh.txt -X "populations: --vcf"
 
